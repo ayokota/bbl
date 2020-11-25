9a1c9186274febdbdfd950de21feb45d4610a993
ayokota <akiyo.yokota@gmail.com>
2020-11-24 21:02:33 -0800

    update profile pic

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -10,18 +10,19 @@ class Contact extends Component {
             <div className="component-team container-fluid">
                 <div className="team-container">
                     <TeamMember 
+                        profilePicUrl="https://media-exp1.licdn.com/dms/image/C4E03AQGQAtZLL3qzxQ/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=qJzejCsMzqOCZDgiLtJ6FeqNsR7oVntDuWN_3ZwmAv8"
                         name="Brett Imel" 
                         position="CEO" 
                         email="brett.imel@bolsternetwork.org"
                         linkedin="https://www.linkedin.com/in/brett-imel-mpa-157634108/"
                     />
                     <TeamMember 
+                        profilePicUrl="https://media-exp1.licdn.com/dms/image/C5603AQHsV6D8xFWj7Q/profile-displayphoto-shrink_400_400/0?e=1611792000&v=beta&t=YbcG-s0MX9kEsi65oe5YPlLyQbTFWLLSn-HCAxpvvT0"
                         name="Akiyo Yokota" 
                         position="Software Engineer" 
                         email="akiyo.yokota@gmail.com"
                         linkedin="https://www.linkedin.com/in/akiyoyokota/"
                     />
-                    <TeamMember name="Person 3" position="Parasite" />
                 </div>
             </div>
         )
--- src/components/Team/TeamMember/TeamMember.jsx
+++ src/components/Team/TeamMember/TeamMember.jsx
@@ -12,7 +12,7 @@ class Contact extends Component {
         return (
             <div className="component-contact-card">
                 <div className="headshot">
-                    <img src="https://ca-times.brightspotcdn.com/dims4/default/22fc870/2147483647/strip/true/crop/960x539+0+0/resize/840x472!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fc9%2F98%2Ffa3b0cb001e3541c5818a89cd5cc%2Fla-1475087498-snap-photo" />
+                    <img src={this.props.profilePicUrl} />
                 </div>
                 <ul>
                     <li className="name">{this.props.name}</li>
