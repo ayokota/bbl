1e1fef376d0cf6e6fb7dc2d89b2d509cc4653242
ayokota <akiyo.yokota@gmail.com>
2020-11-30 17:52:13 -0800

    save

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import HomePage from "./components/HomePage/HomePage";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Footer from "./components/Footer/Footer";
@@ -13,10 +13,12 @@ function App() {
         <Router>
           <div>
             <Banner />
-            <Route path={"/"} exact component={HomePage} />
-            <Route path={"/about"} exact component={About} />
-            <Route path={"/contact"} exact component={Contact} />
-            <Route path={"/team"} exact component={Team} />
+            <div className="body">
+              <Route path={"/"} exact component={HomePage} />
+              <Route path={"/about"} exact component={About} />
+              <Route path={"/contact"} exact component={Contact} />
+              <Route path={"/team"} exact component={Team} />
+            </div>
             <Footer />
           </div>
         </Router>
