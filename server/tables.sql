a0bce5b0c951067c5d6ae38a5b7d1663315783dc
ayokota <akiyo.yokota@gmail.com>
2021-01-24 14:49:30 -0800

    adding user table

---
+++ server/tables.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE `bbl` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+use bbl
+
+CREATE TABLE `sys`.`users` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(60) NOT NULL,
+  `password` VARCHAR(20) NULL,
+  `token` VARCHAR(36) NULL,
+  PRIMARY KEY (`id`, `email`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+
+
