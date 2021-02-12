3501f76751448eda879d58138570640b2298d5ed
ayokota <akiyo.yokota@gmail.com>
2021-02-12 11:15:32 -0800

    made logo clickable

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import "./Banner.scss";
 import {ReactComponent as LogoSvg} from "../../svgs/brand/logo.svg";
 import * as LoginService from "../../services/LoginService.js";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 import * as authAction from "../../redux/action/auth-action.js";
 
 class HomePage extends Component {
@@ -17,8 +17,10 @@ class HomePage extends Component {
 
         return (
             <div className="component-banner container-fluid">
-                <LogoSvg className="logo"/>
-                <div className="title">olster </div>
+                <Link to="/">
+                    <LogoSvg className="logo"/>
+                    <div className="title">olster </div>
+                </Link>
                 {this.props.authenticated === true ? 
                     <div className="logout" onClick={() => this.logout()}>Logout</div> 
                     : 
